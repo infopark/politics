b82fbeed817c9cd05591c2fb29bcf725460ec68c
Tilo Prütz <tilo@infopark.de>
2015-04-24 15:03:10 +0200

    removed dead code

--- lib/politics.rb
+++ lib/politics.rb
@@ -1,18 +1,2 @@
 # encoding: utf-8
 require_relative 'politics/static_queue_worker'
-
-module Politics
-
-  def self.log=(value)
-    @log = log
-  end
-
-  def self.log
-    @log ||= if defined?(RAILS_DEFAULT_LOGGER)
-      RAILS_DEFAULT_LOGGER
-    else
-      require 'logger'
-      Logger.new(STDOUT)
-    end
-  end
-end
